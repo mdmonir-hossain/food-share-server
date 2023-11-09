d19dead6d00a916f4e1cdd29b5099b128211bbc4
mdmonir-hossain <monirsoftengr@gmail.com>
2023-11-09 21:27:54 +0600

    request food

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -59,7 +59,12 @@ async function run() {
         res.send(result);
       });
     
-
+      app.delete("/requestfood/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id:new ObjectId(id)  }
+        const result = await requestfoodCollection.deleteOne(query);
+        res.send(result);
+      });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
